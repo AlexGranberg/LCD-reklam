549c1b1bce19cd86a0207d8af3a7c4383d916333
Daniel Wagenius <wagenius.daniel@gmail.com>
2023-05-25 09:09:51 +0200

    randomcompanydone

--- randomcompany.c
+++ randomcompany.c
@@ -3,4 +3,10 @@
 #include <time.h>
 #include "struct.h"
 
-int randomCompany()
+int randomCompany(int valueIn)
+{
+    if(valueIn < 4) return 1;
+    if(valueIn < 6) return 2;
+    if(valueIn < 8) return 3;
+    if(valueIn < 9) return 4;
+}
--- randomcompany.h
+++ randomcompany.h
@@ -1,6 +1,6 @@
 #ifndef __RANDOM_COMPANY_H
 #define __RANDOM_COMPANY_H
 
-
+int randomCompany(int valueIn);
 
 #endif
