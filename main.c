df801578a19fcc29cdc25f795202c02a48d5f709
Zachral <soderberg.danne@gmail.com>
2023-05-25 11:43:46 +0200

    working code

--- main.c
+++ main.c
@@ -27,92 +27,75 @@ int randomCompany(int valueIn) {
 
 int main(){
     lcd_init();
-
-    
-    //defineCompany(); 
-
-    while(1){
-    
-    //int lastCompany 
-    int randomNum = randomizedNumber(9500);
-    int companyValue = randomCompany(randomNum);
-    //struct randomizedCompany = defineCompany(randomNum); 
-
-    // while(randomizedCompany != lastCompany){
     int num = 0;
     int prevNum = 0;
-    switch (companyValue)
-    {
-    case 1:
-        lcd_printf("Hederlige Harrys Bilar");
-        while(prevNum == num){
-           num = randomizedNumber(3);
-        } 
-        lcd_set_cursor(0,1);
-        if(num == 1){
-            lcd_printf("Köp bil hos Harry");
-        }
-        else if(num == 2){
-            lcd_printf("En god bilaffär (för Harry!)");
-        }
-        else{
-            lcd_printf("Bumblebee? you wish");
-        }
-        _delay_ms(1000);
-        lcd_clear();
-        break;
-    
-    case 2:
-        lcd_printf("Farmor Ankas Pajer AB");
-        while(prevNum == num){
-           num = randomizedNumber(2);
-        }
-        lcd_set_cursor(0,1);
-        if(num == 1){
-            lcd_printf("Köp paj hos Farmor Anka");
-        }
-        else if(num == 2){
-            lcd_printf("Skynda innan Mårten ätit alla pajer");
-        }
-        _delay_ms(1000);
-        lcd_clear();
-        break;
 
-    case 3:
-        lcd_printf("Svarte Petters Svartbyggen");
-        while(prevNum == num){
-           num = randomizedNumber(2);
-        }
-        lcd_set_cursor(0,1);
-        if(num == 1){
-            lcd_printf("Låt Petter bygga åt dig");
-        }
-        else if(num == 2){
-            lcd_printf("Bygga svart? Ring Petter");
-        }
-        _delay_ms(1000);
-        lcd_clear();
-        break;
-    case 4:
-        lcd_printf("IOT:s REklambyrå");
-        num = randomizedNumber(1);
-        lcd_set_cursor(0,1);
-        if(num == 1){
-            lcd_printf("Synas här? IOT:s Reklambyrå");
-        }
-        _delay_ms(1000);
-        lcd_clear();
-        break;
+    while(1){
+    int randomNum = randomizedNumber(9500);
+    int companyValue = randomCompany(randomNum);
+    while(prevNum != companyValue){
+        prevNum = companyValue;
+        switch (companyValue)
+        {
+        case 1:
+            lcd_printf("Hederlige Harrys Bilar");
+            num = randomizedNumber(3); 
+            lcd_set_cursor(0,1);
+            if(num == 1){
+                lcd_printf("Köp bil hos Harry");
+            }
+            else if(num == 2){
+                lcd_printf("En god bilaffär (för Harry!)");
+            }
+            else{
+                lcd_printf("Bumblebee? you wish");
+            }
+            _delay_ms(2000);
+            lcd_clear();
+            break;
+        
+        case 2:
+            lcd_printf("Farmor Ankas Pajer AB");
+            num = randomizedNumber(2);
+            lcd_set_cursor(0,1);
+            if(num == 1){
+                lcd_printf("Köp paj hos Farmor Anka");
+            }
+            else if(num == 2){
+                lcd_printf("Skynda innan Mårten ätit alla pajer");
+            }
+            _delay_ms(2000);
+            lcd_clear();
+            break;
 
-    default:
-        break;
+        case 3:
+            lcd_printf("Svarte Petters Svartbyggen");
+            num = randomizedNumber(2);
+            lcd_set_cursor(0,1);
+            if(num == 1){
+                lcd_printf("Låt Petter bygga åt dig");
+            }
+            else if(num == 2){
+                lcd_printf("Bygga svart? Ring Petter");
+            }
+            _delay_ms(2000);
+            lcd_clear();
+            break;
+        case 4:
+            lcd_printf("IOT:s REklambyrå");
+            num = randomizedNumber(1);
+            lcd_set_cursor(0,1);
+            if(num == 1){
+                lcd_printf("Synas här? IOT:s Reklambyrå");
+            }
+            _delay_ms(2000);
+            lcd_clear();
+            break;
+
+        default:
+            break;
+        }
     }
-
-    //    if company:
-    //      num = randomizeNumber(numOfCommercial);
-    //      print(commercial); 
-    // 
-    //}
     }
 
     return 0;
