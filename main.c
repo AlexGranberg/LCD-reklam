5f2750c0a6bfb6b085455653dc59e92ae8bd8a53
AlexGranberg <alexandergranberg1983@gmail.com>
2023-05-25 08:17:53 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "uart.h"
+#include "millis.h"
+
+
+
+#define BIT_SET(a, b) ((a) |= (1ULL << (b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+
+
+int main(void){
+    init_serial();
+    millis_init();
+    sei();
+
+
+    while(1){
+    }
+
+    return 0;
+}
